9c0ad0e73a920599bfdb71fc9d4e9cf89f97b7f2
s4p3r-c1n0s <umang.cdr.90@gmail.com>
2011-09-11 04:30:05 +0530

    trial5_bitly

--- content_bitly.js
+++ content_bitly.js
@@ -8,7 +8,7 @@
 
     options : function() {
       config.api.log("Options!");
- $j(document).ready(function () {
+/* $j(document).ready(function () {
        // debugger
        $j(":wijmo-wijdialog").wijdialog("destroy").remove();
               $j('#dialog').wijdialog({
@@ -17,8 +17,62 @@
                                   refresh: { visible: false }
                                               }
                                       });
-                               });
-                             },
+                               });*/
+createDom : function() {
+      $j("#custom-rss-form").remove();  // cleanup any old invocation...
+
+      var form = $j("<div/>", {id : "custom-rss-form"}).html(
+                  "<center>\
+                    <form>\
+                      <div style='margin-top:10px;'>\
+                        <span style='color:#A0A0A0; padding-right:5px;'>(Include http://) </span>\
+                        <input type='text' size='40' id='custom-rss-url' style='font-size:16px; padding:2px; border:1px solid grey;'>\
+                        <p style='margin-top:10px; font-size:12px;'><button id='custom-rss-save'>Save Feed</button</p>\
+                      </div>\
+                    </form>\
+                  </center>\
+                  "
+                ).appendTo($j("body"));
+
+      $j("#custom-rss-url").val(config.api.getData("feedUrl") || "");
+
+      $j("#custom-rss-save").button({
+        icons: {
+          primary: "ui-icon-disk"
+        }
+      })
+      .click(function(event) {
+        var feedUrl = $j("#custom-rss-url").val();
+        if(CustomRss.isValidFeedUrl(feedUrl) == true)
+        {
+          config.api.setData("feedUrl", feedUrl);
+          CustomRss.watchFeed(feedUrl);
+        }
+        $j("#custom-rss-form").wijdialog("close");
+        return false;
+      });
+
+      form.wijdialog({width : 500, title : "Add an RSS Feed URL"});
+    },
+
+
+
+    // Returns true if the feedUrl is found to be valid
+       isValidFeedUrl : function(url) {
+       return ((url) && (url.length > 0) && ((url.indexOf("http://") != -1) || (url.indexOf("https://") != -1)))
+                   },
+    
+    
+                       // creates the IJFeedWatcherFactory object.
+       // Url can be escaped or unescaped feedUrl
+       watchFeed : function(url) {
+       try {
+       new IJFeedWatcherFactory(unescape(url), {image : "http://cs.adomado.com/custom_rss/rss.png", timeoutMinutes : 5, callback : function(entry) {
+       config.api.createPanelNotification({image : "http://cs.adomado.com/custom_rss/rss.png", text : entry.title, url : entry.link});
+       }});
+       } catch(e) {}
+       }
+       },
 
     contextAction : function() {
       config.api.log("Context action invoked");
