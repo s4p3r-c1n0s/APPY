6dfebbdfcd4e3abdcb74a0c4829efdea31c0e1d4
s4p3r-c1n0s <umang.cdr.90@gmail.com>
2011-09-13 20:38:48 +0530

    low

--- manifest_shoRtLin.js
+++
@@ -1,9 +0,0 @@
-_adomado_prefs_apps["manifests"]["__APP_ID__"] = {
-  "name" : "shoRtLin",
-  "version" : "1.0.0.1",
-  "runOnDomains" : [
-                      {
-                        "domain" : "*"
-                      }
-                    ]
-};
