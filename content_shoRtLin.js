b7f41fe956e258c5d271aa2d0a717e4c24665f00
s4p3r-c1n0s <umang.cdr.90@gmail.com>
2011-09-18 23:36:37 +0530

    LIN1

--- content_shoRtLin.js
+++ content_shoRtLin.js
@@ -17,7 +17,7 @@ createDom : function() {
       var form = $j("<div/>", {id : "shoRtLin-form"}).html(
                   "<center>\
 		<div id='panel' style='width:100px;height:100px;background:red;'>\
-			<a id='close' href="#"></a>\
+			<a id='close' style='display:block;float:right;width:30px;height:29px;background:url(http://digitalsbykobke.com/images/close.png) no-repeat center center;' href='#'></a>\
 		</div>\
 		<div id='shoRtLin-tab' style='position:fixed; width:500px; left: 400px; top: 150px'>\
 			<ul>\
