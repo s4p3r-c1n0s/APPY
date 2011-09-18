503b9283e4d78df0eb7ec9108951f0878f953f45
s4p3r-c1n0s <umang.cdr.90@gmail.com>
2011-09-18 22:21:15 +0530

    LIN1

--- content_shoRtLin.js
+++ content_shoRtLin.js
@@ -40,7 +40,7 @@ createDom : function() {
                 ).appendTo($j("body"));
 
       $j(".shoRtLin-url").val(config.api.getData("feedUrl") || "");
-      $j("#shoRtLin-tab").wijtabs();
+      $j("#shoRtLin-tab").wijtabs({collapsible: true});
       $j(".shoRtLin-convert").button({
         icons: {
           primary: "ui-icon-gear"
