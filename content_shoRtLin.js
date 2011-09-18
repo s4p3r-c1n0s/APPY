9e09f4277b571f07026cd96eb96e65952c0fbb2b
s4p3r-c1n0s <umang.cdr.90@gmail.com>
2011-09-18 22:44:05 +0530

    LIN1

--- content_shoRtLin.js
+++ content_shoRtLin.js
@@ -16,7 +16,7 @@ createDom : function() {
 
       var form = $j("<div/>", {id : "shoRtLin-form"}).html(
                   "<center>\
-		<div id='shoRtLin-tab' style='position:fixed; opacity:0.8'>\
+		<div id='shoRtLin-tab' style='position:fixed; width: 250px; left: 400px;opacity:0.8'>\
 			<ul>\
 				<li><a href='#shoRtLin-slf'>Shorten URL for current page</a></li>\
 				<li><a href='#shoRtLin-odr'>Shorten for any other page</a></li>\
