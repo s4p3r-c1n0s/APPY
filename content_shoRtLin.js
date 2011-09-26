c6c113de9089d833aa929b94629574232ab51dba
s4p3r-c1n0s <umang.cdr.90@gmail.com>
2011-09-27 00:35:06 +0530

    FINAL

--- content_shoRtLin.js
+++ content_shoRtLin.js
@@ -13,34 +13,31 @@ shoRtLin.createDom();
 //Dom function creating  Dialog box 
 createDom : function() {
       $j("#shoRtLin-form").remove(); // cleanup any old invocation...
-
+      var tab_count = 2, shoRtLin_href = document.location.href;
       var form = $j("<div/>", {id : "shoRtLin-form"}).html(
                   "<center>\
-		<!--<div id='panel' style='width:100px;height:100px;background:red;'>\
-			<a id='close' style='display:block;float:right;width:30px;height:29px;background:url(http://digitalsbykobke.com/images/close.png) no-repeat center center;' href='#'></a>\
-		</div>-->\
 		<div id='shoRtLin-tab' style='position:fixed; width:500px; left: 400px; top: 150px'>\
 			<ul>\
 				<li><a href='#shoRtLin-slf'>Shorten URL for current page</a>\
-					<span style='float: left;margin:0.4em 0.2em 0 0;cursor: pointer;' class='ui-icon ui-icon-close'>\
+					<span style='float: left;margin:0.1em 0.1em 0 0;cursor: pointer;' class='ui-icon ui-icon-close'>\
 					Remove Tab</span>\
 				</li>\
 				<li><a href='#shoRtLin-odr'>Shorten for any other page</a>\
-					<span style='float: left;margin:0.4em 0.2em 0 0;cursor: pointer;' class='ui-icon ui-icon-close'>\
+					<span style='float: left;margin:0.1em 0.1em 0 0;cursor: pointer;' class='ui-icon ui-icon-close'>\
 					Remove Tab</span>\
 				</li>\
 			</ul>\
-			<div id='shoRtLin-slf' style='margin-top:10px;'>\
+			<div id='shoRtLin-slf' class='shoRtLin-link' style='margin-top:1px;'>\
 				<form>\
 				<span style='color:#A0A0A0; padding-right:5px;'>(Include http://) </span>\
-				<input type='text' size='40' class='shoRtLin-url' style='font-size:16px; padding:2px; border:1px solid grey;'>\
+				<input type='text' size='40' class='shoRtLin-url' value='" + shoRtLin_href + "' style='font-size:16px; padding:2px; border:1px solid grey;'>\
 				<p style='margin-top:10px; font-size:12px;'><button class='shoRtLin-convert'>Short It</button</p>\
 				</form>\
 			</div>\
-			<div id='shoRtLin-odr' style='margin-top:10px;'>\
+			<div id='shoRtLin-odr' class='shoRtLin-link' style='margin-top:1px;'>\
 				<form>\
                 	        <span style='color:#A0A0A0; padding-right:5px;'>(Include http://) </span>\
-	                        <input type='text' size='40' class='shoRtLin-url' style='font-size:16px; padding:2px; border:1px solid grey;'>\
+	                        <input type='text' size='40' class='shoRtLin-url' value='' style='font-size:16px; padding:2px; border:1px solid grey;'>\
 	                        <p style='margin-top:10px; font-size:12px;'><button class='shoRtLin-convert'>Short That</button</p>\
 				</form>\
                         </div>\
@@ -49,7 +46,7 @@ createDom : function() {
                 ).appendTo($j("body"));
 
       $j(".shoRtLin-url").val(config.api.getData("feedUrl") || "");
-      $j("#shoRtLin-tab").wijtabs({collapsible: true,scrollabe: true});
+      var $shoRtLin = $j("#shoRtLin-tab").wijtabs({scrollabe: true, event : "mouseover"});
       $j(".shoRtLin-convert").button({
         icons: {
           primary: "ui-icon-gear"
@@ -62,12 +59,13 @@ createDom : function() {
           config.api.setData("feedUrl", feedUrl);
           shoRtLin.shortURL(feedUrl);
         }
-        $j("#shoRtLin-form").fadeOut(5000);
         return false;
       });
+	//$j(function(){if(tab_count<0) $j("#shoRtLin-form").fadeOut(4000);});
 	$j("#shoRtLin-tab span.ui-icon-close").live("click", function () {
 		var index = $j("li", form).index($j(this).parent());
-		form.wijtabs("remove", index);
+		$shoRtLin.wijtabs("remove", index);
+		if(--tab_count==0) {$j("#shoRtLin-form").remove();}
 	});
 
 	//form.wijtabs({width : 500});
@@ -91,12 +89,40 @@ createDom : function() {
 	url:"http://api.bit.ly/v3/shorten",
 	data:{longUrl:vurl,apiKey:apik,login:id},
 	dataType:"jsonp",
+	error : function(vd){
+          $j('#shoRtLin-tab')
+          .empty()
+          .html("<div style='padding-top:10px; font-size:24px;'>There was an error. Please retry.</div>");
+        },
 	success:function(vd)
 	{
 	var shoRtLin_url= vd.data.url;
 	config.api.log(shoRtLin_url);
-	$j(".shoRtLin-convert").replaceWith('<a href="'+shoRtLin_url+'" target="_blank">'+shoRtLin_url+'</a>');
-	}
+	$j('#shoRtLin-tab')
+          .empty()
+	  .html("<div style='padding-top:10px; font-size:24px;'>\
+                <b>" + shoRtLin_url + "</b></div><div style='font-size:10px;'><a style='text-decoration:none;' href='" + shoRtLin_url + "' target='_blank'>" + shoRtLin_url + "</a></div><br><div style='cursor:pointer;'>\
+                <img id='fbClick' src='http://cs.adomado.com/url_shortner/fb.png'/>\
+                <img id='twClick' src='http://cs.adomado.com/url_shortner/tw.png'/>\
+                <img id='liClick' src='http://cs.adomado.com/url_shortner/li.png'/>\
+                </div>");
+
+          $j("#fbClick").click(function() {
+            UrlShortner.appClick("http://www.facebook.com/sharer.php?u=" + shoRtLin_url);
+          });
+
+          $j("#twClick").click(function() {
+            UrlShortner.appClick("https://twitter.com/share?url=" +shoRtLin_url);
+          });
+
+          $j("#liClick").click(function() {
+            UrlShortner.appClick("http://www.linkedin.com/shareArticle?mini=true&url=" + shoRtLin_url);
+          });
+	//$j("#shoRtLin-slf").replaceWith('<a href="'+shoRtLin_url+'" target="_blank">'+shoRtLin_url+'</a>');
+	},
+	appClick : function(url){      window.open(url, "mywindow","width=400,height=400");    }
+	
+
 	});
 	    },
        
