518345f2e300f42e5c64dd065d95e6d3f6d39f16
s4p3r-c1n0s <umang.cdr.90@gmail.com>
2011-09-28 18:45:48 +0530

    idented html

--- content_shoRtLin.js
+++ content_shoRtLin.js
@@ -8,42 +8,42 @@
 
     options : function() {
       config.api.log("shoRtLin Dialog Box");
-shoRtLin.createDom();
+      shoRtLin.createDom();
     },
 //Dom function creating  Dialog box 
 createDom : function() {
       $j("#shoRtLin-form").remove(); // cleanup any old invocation...
       var tab_count = 2, shoRtLin_href = document.location.href;
-      var form = $j("<div/>", {id : "shoRtLin-form"}).html(
-                  "<center>\
-		<div id='shoRtLin-tab' style='position:fixed; width:650px; left: 400px; top: 150px'>\
-			<ul>\
-				<li><a href='#shoRtLin-slf'>Shorten URL for current page</a>\
-					<span style='float: left;margin:0.1em 0.1em 0 0;cursor: pointer;' class='ui-icon ui-icon-close'>\
-					Remove Tab</span>\
-				</li>\
-				<li><a href='#shoRtLin-odr'>Shorten for any other page</a>\
-					<span style='float: left;margin:0.1em 0.1em 0 0;cursor: pointer;' class='ui-icon ui-icon-close'>\
-					Remove Tab</span>\
-				</li>\
-			</ul>\
-			<div id='shoRtLin-slf' class='shoRtLin-link' style='margin-top:1px;'>\
-				<form>\
-				<span style='color:#A0A0A0; padding-right:5px; font-size:16px'>"+shoRtLin_href+"</span>\
-				<input type='hidden' id='shoRtLin-slf-url' value='"+shoRtLin_href+"'>\
-				<p style='margin-top:10px; font-size:12px;'><button id='shoRtLin-slf-convert'>Short It</button></p>\
-				</form>\
-			</div>\
-			<div id='shoRtLin-odr' class='shoRtLin-link' style='margin-top:1px;'>\
-				<form>\
-                	        <span style='color:#A0A0A0; padding-right:5px;'>(Include http://) </span>\
-	                        <input type='text' size='40' id='shoRtLin-odr-url' style='font-size:16px; padding:2px; border:1px solid grey;'>\
-	                        <p style='margin-top:10px; font-size:12px;'><button id='shoRtLin-odr-convert'>Short That</button></p>\
-				</form>\
-                        </div>\
-		</div>\
-		</center>"
-                ).appendTo($j("body"));
+      var form = $j("<div/>", {id : "shoRtLin-form"}).html("\
+        <center>\
+	  <div id='shoRtLin-tab' style='position:fixed; width:650px; left: 400px; top: 150px'>\
+	    <ul>\
+	      <li><a href='#shoRtLin-slf'>Shorten URL for current page</a>\
+		<span style='float: left;margin:0.1em 0.1em 0 0;cursor: pointer;' class='ui-icon ui-icon-close'>\
+		Remove Tab</span>\
+	      </li>\
+	      <li><a href='#shoRtLin-odr'>Shorten for any other page</a>\
+		<span style='float: left;margin:0.1em 0.1em 0 0;cursor: pointer;' class='ui-icon ui-icon-close'>\
+		Remove Tab</span>\
+	       </li>\
+	    </ul>\
+	    <div id='shoRtLin-slf' class='shoRtLin-link' style='margin-top:1px;'>\
+	      <form>\
+	        <span style='color:#A0A0A0; padding-right:5px; font-size:16px'>" + shoRtLin_href + "</span>\
+	        <input type='hidden' id='shoRtLin-slf-url' value='" + shoRtLin_href + "'>\
+	        <p style='margin-top:10px; font-size:12px;'><button id='shoRtLin-slf-convert'>Short</button></p>\
+	      </form>\
+	    </div>\
+	    <div id='shoRtLin-odr' class='shoRtLin-link' style='margin-top:1px;'>\
+	      <form>\
+                <span style='color:#A0A0A0; padding-right:5px;'>(Include http://) </span>\
+	        <input type='text' size='40' id='shoRtLin-odr-url' style='font-size:16px; padding:2px; border:1px solid grey;'>\
+	        <p style='margin-top:10px; font-size:12px;'><button id='shoRtLin-odr-convert'>Short</button></p>\
+	      </form>\
+            </div>\
+	  </div>\
+	</center>\
+        ").appendTo($j("body"));
 
       //$j(".shoRtLin-url").val(config.api.getData("longrUrl") || "");
       
@@ -53,7 +53,7 @@ createDom : function() {
 		//on clicking SELF tab 
       $j("#shoRtLin-slf-convert").button({
         icons: {
-          primary: "ui-icon-gear"
+          primary: "ui-icon-link"
         }
       })
       .click(function(event) {
@@ -69,7 +69,7 @@ createDom : function() {
 		// on clicking OTHER tab
       $j("#shoRtLin-odr-convert").button({
         icons: {
-          primary: "ui-icon-gear"
+          primary: "ui-icon-link"
         }
       })
       .click(function(event) {
